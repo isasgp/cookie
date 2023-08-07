6c37da385ed5eac004b7e46d2dda126fcadf54e6
isasgp <isasgp@naver.com>
2023-08-07 15:22:23 +0900

    PermissionActivity.java 코드 수정

--- app/src/main/java/com/example/cookie/PermissionActivity.java
+++ app/src/main/java/com/example/cookie/PermissionActivity.java
@@ -13,13 +13,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+/*
+사용자가 권한 거부시 앱 종료 또는 다른 방안 생각해두기
+REQUEST_ALLOW 전역 변수를 통해 권한 요청 여부 제어 가능
+*/
 public class PermissionActivity extends AppCompatActivity {
-
-
+/*
     private static final int REQUEST_CAMERA_PERMISSION = 1001;
     private static final int REQUEST_STORAGE_PERMISSION = 1002;
     private static final int REQUEST_LOCATION_PERMISSION = 1003;
-    private static final int REQUEST_ALL_PERMISSIONS = 1004;
+*/
+    private static final int REQUEST_ALL_PERMISSIONS = 1001;
+    private static boolean REQUEST_ALLOW = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +39,14 @@ public class PermissionActivity extends AppCompatActivity {
             public void run() {
                 requestAllPermissions();
             }
-        },1000);	//700밀리 초 동안 딜레이
+        },1000);	//1000밀리 초 동안 딜레이
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(PermissionActivity.this, LoginActivity.class);
                 startActivity(intent);
+
             }
         });
     }
@@ -51,23 +58,25 @@ public class PermissionActivity extends AppCompatActivity {
                 android.Manifest.permission.ACCESS_FINE_LOCATION
         };
 
-        boolean allPermissionsGranted = true;
-
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                allPermissionsGranted = false;
-                break;
-            }
-        }
-
-        if (!allPermissionsGranted) {
+        if (!checkAllPermissions(permissions)) {
             ActivityCompat.requestPermissions(this, permissions, REQUEST_ALL_PERMISSIONS);
-        } else {
-            // 이미 모든 권한이 허용되었을 경우, 해당 권한들을 사용하여 작업을 수행하면 됩니다.
+
         }
+
     }
 
+    private boolean checkAllPermissions (String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                REQUEST_ALLOW = false;
+                return false;
+            }
+        }
+        REQUEST_ALLOW = true;
+        return true;
+    }
     // 권한 요청 결과 처리
+    /*
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -88,6 +97,8 @@ public class PermissionActivity extends AppCompatActivity {
                 // 위치 권한에 대한 처리도 동일하게 구현합니다.
                 break;
             // 다른 권한들이 추가된 경우에도 이와 같이 처리합니다.
+
         }
     }
+    */
 }
