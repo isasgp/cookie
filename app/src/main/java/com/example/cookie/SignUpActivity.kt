9498558377efc5ddcd6c12b8a1a6885bbef29ee3
박슬기 <green7170@swu.ac.kr>
2023-08-07 11:39:45 +0900

    회원가입 화면 구현

--- app/src/main/java/com/example/cookie/SignUpActivity.kt
+++ app/src/main/java/com/example/cookie/SignUpActivity.kt
@@ -1,6 +1,10 @@
 package com.example.cookie
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,8 +17,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.cookie.ui.theme.CookieTheme
 
 class SignUpActivity : ComponentActivity() {
+
+    private lateinit var edtId: EditText // 아이디 입력창
+    private lateinit var edtPassword: EditText // 비밀번호 입력창
+    private lateinit var edtPasswordCheck: EditText // 비밀번호 확인 입력창
+    private lateinit var btnSignUp: Button // 회원가입 버튼
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        edtId = findViewById(R.id.edt_id)
+        edtPassword = findViewById(R.id.edt_pass)
+        edtPasswordCheck = findViewById(R.id.edt_passck)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+        btnSignUp.setOnClickListener {
+            Toast.makeText(this@SignUpActivity, "회원가입 성공", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
