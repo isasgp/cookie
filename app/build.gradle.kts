bdf4a4fbc4b4c7e92fa9bd03e8e81f6016115f10
박슬기 <green7170@swu.ac.kr>
2023-08-09 17:54:27 +0900

    강아지 정보, 피부질환 정보 액티비티 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
     implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
 
     annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
---
+++ app/src/main/java/com/example/cookie/SkinInfoActivity.kt
@@ -0,0 +1,10 @@
+package com.example.cookie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SkinInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
