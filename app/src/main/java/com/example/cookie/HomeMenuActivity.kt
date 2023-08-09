15d0b2bdfaf9d80eef6f04c676ad425f1992161d
박슬기 <green7170@swu.ac.kr>
2023-08-09 16:54:11 +0900

    홈 메뉴 화면 코드 수정

--- app/src/main/java/com/example/cookie/HomeMenuActivity.kt
+++ app/src/main/java/com/example/cookie/HomeMenuActivity.kt
@@ -1,5 +1,7 @@
 package com.example.cookie
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,18 +9,22 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.LinearLayout
-import androidx.appcompat.widget.ButtonBarLayout
 
 
 class HomeMenuActivity : AppCompatActivity() {
 
-    var isPageOpen : Boolean = false
+    var ishomePageOpen : Boolean = false
     lateinit var LeftAnim : Animation
     lateinit var RightAnim : Animation
-    lateinit var button : Button
-    lateinit var page : LinearLayout
+    lateinit var btnHome : Button
+    lateinit var homePage : LinearLayout
 
+    private lateinit var btnMenu1: Button
+    private lateinit var btnMenu2: Button
+    private lateinit var btnMenu3: Button
+    private lateinit var btnMenu4: Button
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homemenu)
@@ -26,35 +32,61 @@ class HomeMenuActivity : AppCompatActivity() {
         LeftAnim = AnimationUtils.loadAnimation(this, R.anim.translate_left)
         RightAnim = AnimationUtils.loadAnimation(this,R.anim.translate_right)
 
-        LeftAnim.setAnimationListener(SlidingPageAnimationListener())
-        RightAnim.setAnimationListener(SlidingPageAnimationListener())
+        LeftAnim.setAnimationListener(SlidinghomePageAnimationListener())
+        RightAnim.setAnimationListener(SlidinghomePageAnimationListener())
 
-        button = findViewById(R.id.button)
-        page = findViewById(R.id.page)
+        btnHome = findViewById(R.id.btn_home)
+        homePage = findViewById(R.id.page)
 
-        button.setOnClickListener {
-            if (isPageOpen) {
-                page.startAnimation(RightAnim)
+        btnMenu1 = findViewById(R.id.btn_home_menu1)
+        btnMenu2 = findViewById(R.id.btn_home_menu2)
+        btnMenu3 = findViewById(R.id.btn_home_menu3)
+        btnMenu4 = findViewById(R.id.btn_home_menu4)
+
+
+        btnMenu1.setOnClickListener {
+            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnMenu2.setOnClickListener {
+            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnMenu3.setOnClickListener {
+            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnMenu4.setOnClickListener {
+            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnHome.setOnClickListener {
+            if (ishomePageOpen) {
+                homePage.startAnimation(RightAnim)
             }
             else{
-                page.visibility = View.VISIBLE
-                page.startAnimation(LeftAnim)
+                homePage.visibility = View.VISIBLE
+                homePage.startAnimation(LeftAnim)
             }
         }
     }
 
-    private inner class SlidingPageAnimationListener : Animation.AnimationListener {
+    private inner class SlidinghomePageAnimationListener : Animation.AnimationListener {
 
         override fun onAnimationEnd(animation: Animation?) : Unit {
-            if(isPageOpen){
-                page.visibility=View.INVISIBLE
+            if(ishomePageOpen){
+                homePage.visibility=View.INVISIBLE
 
-                button.text="메뉴 보기"
-                isPageOpen = false
+                btnHome.text="메뉴 보기"
+                ishomePageOpen = false
             }
             else{
-                button.text="닫기"
-                isPageOpen = true
+                btnHome.text="닫기"
+                ishomePageOpen = true
             }
         }
 
@@ -66,46 +98,3 @@ class HomeMenuActivity : AppCompatActivity() {
         }
     }
 }
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.widget.Button
-//import androidx.appcompat.app.AppCompatActivity
-//
-//class HomeMenuActivity : AppCompatActivity() {
-//
-//    private lateinit var btnMenu1: Button
-//    private lateinit var btnMenu2: Button
-//    private lateinit var btnMenu3: Button
-//    private lateinit var btnMenu4: Button
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_homemenu)
-//
-//        btnMenu1 = findViewById(R.id.btn_home_menu1)
-//        btnMenu2 = findViewById(R.id.btn_home_menu2)
-//        btnMenu3 = findViewById(R.id.btn_home_menu3)
-//        btnMenu4 = findViewById(R.id.btn_home_menu4)
-//
-//
-//        btnMenu1.setOnClickListener {
-//            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        btnMenu2.setOnClickListener {
-//            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        btnMenu3.setOnClickListener {
-//            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        btnMenu4.setOnClickListener {
-//            val intent = Intent(this@HomeMenuActivity, LoadingActivity::class.java)
-//            startActivity(intent)
-//        }
-//    }
-//}
