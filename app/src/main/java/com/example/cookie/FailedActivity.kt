688217e9708807065e4c17f70d40939445932696
isasgp <isasgp@naver.com>
2023-08-11 15:10:29 +0900

    FailedActivity 버튼 연결

--- app/src/main/java/com/example/cookie/FailedActivity.kt
+++ app/src/main/java/com/example/cookie/FailedActivity.kt
@@ -8,14 +8,28 @@ import androidx.appcompat.app.AppCompatActivity
 class FailedActivity : AppCompatActivity() {
 
     lateinit var btnHome : Button
+    lateinit var btnRetry : Button
+    lateinit var btnGuide : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_failed)
 
         btnHome = findViewById(R.id.btn_home)
+        btnRetry = findViewById(R.id.btn_retry)
+        btnGuide = findViewById(R.id.btn_guide)
+
         btnHome.setOnClickListener {
             val intent = Intent(this@FailedActivity, HomeMenuActivity::class.java)
             startActivity(intent)
         }
+        btnRetry.setOnClickListener {
+            val intent = Intent(this@FailedActivity, CameraViewActivity::class.java)
+            startActivity(intent)
+        }
+        btnGuide.setOnClickListener {
+            val intent = Intent(this@FailedActivity, GuideActivity1::class.java)
+            startActivity(intent)
+        }
+
     }
 }
