c58fb4c506c920441c06576f681470649e6ebeac
isasgp <isasgp@naver.com>
2023-08-11 13:46:35 +0900

    가이드 액티비티 생성

---
+++ app/src/main/java/com/example/cookie/GuideActivity2.java
@@ -0,0 +1,16 @@
+package com.example.cookie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cookie.R;
+
+public class GuideActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide2);
+    }
+}
