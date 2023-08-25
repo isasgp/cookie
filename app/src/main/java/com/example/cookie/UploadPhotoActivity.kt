4c73aa56025002b70a5c2d874c96891386e4a920
박슬기 <green7170@swu.ac.kr>
2023-08-25 10:43:59 +0900

    카메라 업로드

--- app/src/main/java/com/example/cookie/UploadPhotoActivity.kt
+++ app/src/main/java/com/example/cookie/UploadPhotoActivity.kt
@@ -31,18 +31,17 @@ class UploadPhotoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_upload_photo)
 
         // 이거 자바 코드 그대로 코틀린 변환 버튼만 누른건데 이런 형식으로 쓰면 될듯
-        val intent = intent
-        val img_name = intent.getStringExtra("image_name")     // 앞 액티비티인 ImageViewActivity에서 putExtra 해준거 받아오는 코드
+           // 앞 액티비티인 ImageViewActivity에서 putExtra 해준거 받아오는 코드
                                                                     // CamerViewActivity에서 찍은 이미지 이름 받아오는 것
-        val photoFile = File(getExternalFilesDir(null), img_name)   // 파일 위치, 파일 이름으로 파일 받아와서 생성 하는 코드
+        // val photoFile = File(getExternalFilesDir(null), img_name)   // 파일 위치, 파일 이름으로 파일 받아와서 생성 하는 코드
                                                                          // getExternalFilesDir(null) 메소드가 앱 설치시 자동으로 생성되는 앱 내부의 절대 공간에 접근하는 메소드 외부에서는 보이지 않음
                                                                          // 아마 이 파일이 .jpg 파일 그대로 일 듯
         // 이 밑으로 파일 처리해서 Upload 구현하면 될 듯 (파이팅!!)
 
 
         // 갤러리에서 사진 선택
-        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(galleryIntent, REQUEST_IMAGE_PICK)
+//        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//        startActivityForResult(galleryIntent, REQUEST_IMAGE_PICK)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -69,8 +68,12 @@ class UploadPhotoActivity : AppCompatActivity() {
         try {
             // 선택한 이미지의 실제 경로 가져오기
             val inputStream: InputStream = contentResolver.openInputStream(imageUri)!!
-            val file = File(cacheDir, "temp_image.jpg")
-            // val file = File(getExternalFilesDir(null), "temp_image.jpg")
+            // val file = File(cacheDir, "temp_image.jpg")
+            val intent = intent
+            val img_name = intent.getStringExtra("image_name")
+            val file = File(getExternalFilesDir(null), img_name)
+            val temp_file = File("temp_image.jpg")
+            file.renameTo(temp_file)
             val fileOutputStream = file.outputStream()
             inputStream.copyTo(fileOutputStream)
             fileOutputStream.close()
